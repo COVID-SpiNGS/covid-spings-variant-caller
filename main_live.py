e0df268e49e9b4f4ccd007fab301c967d4ace471
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-10-12 19:08:47 +0200

    Added functions and functionality

--- main_live.py
+++ main_live.py
@@ -8,6 +8,7 @@ import argparse
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+parser = argparse.ArgumentParser()
 
 
 def handle_client(sock):
@@ -30,8 +31,6 @@ def serve_forever():
 
 
 
-parser = argparse.ArgumentParser()
-
 def construct_cli():
     group = parser.add_mutually_exclusive_group()
     group.add_argument("start", nargs='?', help="echo the string you use here")
@@ -40,15 +39,67 @@ def construct_cli():
     group.add_argument("write", nargs='?', help="echo the string you use here")
 
 
+def _process_bam(path: str):
+    return True
+
+def _write_vcf(path: str):
+    return True
+
+def _shutdown_gracefully():
+    print('Stopping server in 10 seconds...')
+    time.sleep(10)
+    return True
+
 if __name__ == '__main__':
 
+    sock_running = False
     construct_cli()
 
     args = parser.parse_args()
 
-    if args.start:
-        print("hahahah")
-    print(args.start)
+    if args.start is not None and sock_running is False:
+        sock_running = True
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind((HOST, PORT))
+            sock.listen()
+            print(f'Running now under {HOST}:{PORT}...')
+
+            while True:
+                connection, address = sock.accept()
+                with connection:
+                    task = connection.recv(1024)
+                    print(f"Received {task!r}")
+
+                    if task == b'stop':
+                        _shutdown_gracefully()
+
+                    elif task == b'process':
+                        print('Processing BAM...', params)
+                        _process_bam(params)
+                    elif task == b'write':
+                        print('Write VCF...', params)
+                        _write_vcf(params)
+                    elif task == b'save':
+                        print('Save Checkpoint...')
+                    elif task == b'load':
+                        print('Load Checkpoint...')
+
+                    else:
+                        print("NO SUCH ACTION")
+                        # TODO: Throw Illegal Action Exception maybe ?
+
+
+
+
+    if args.stop is not None and sock_running is True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((HOST, PORT))
+            sock.sendall(b'stop')
+            sock.close()
+
+
+
+
 
     #if args.start is True:
    #     print("start")
