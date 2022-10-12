cf8034b37c69ec408005fbf48642d7f696fd4e28
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-10-12 20:35:39 +0200

    Added logging

--- main_live.py
+++ main_live.py
@@ -3,11 +3,17 @@ import socket
 import time
 import threading
 import daemon
+import logging
 import argparse
+
+logging.basicConfig(filename='vcf.log',
+level=logging.DEBUG,
+format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
 
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
 parser = argparse.ArgumentParser()
 
 
@@ -40,14 +46,17 @@ def construct_cli():
 
 
 def _process_bam(path: str):
+    logging.info('Processing BAM...', path)
     return True
 
 def _write_vcf(path: str):
     return True
 
-def _shutdown_gracefully():
-    print('Stopping server in 10 seconds...')
+def _shutdown_gracefully(sock):
+    logging.info('Stopping server in 10 seconds...')
     time.sleep(10)
+    sock.shutdown(socket.SHUT_RDWR)
+    sock.close()
     return True
 
 if __name__ == '__main__':
@@ -58,52 +67,37 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.start is not None and sock_running is False:
-        sock_running = True
+        #sock_running = True
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.bind((HOST, PORT))
             sock.listen()
-            print(f'Running now under {HOST}:{PORT}...')
+            logging.info(f'Running now under {HOST}:{PORT}...')
 
             while True:
                 connection, address = sock.accept()
                 with connection:
-                    task = connection.recv(1024)
-                    print(f"Received {task!r}")
+                    data = connection.recv(1024)
+                    logging.info(f"Received {data!r}")
+                    action = b''
+                    params = b''
 
-                    if task == b'stop':
-                        _shutdown_gracefully()
-
-                    elif task == b'process':
-                        print('Processing BAM...', params)
-                        _process_bam(params)
-                    elif task == b'write':
-                        print('Write VCF...', params)
-                        _write_vcf(params)
-                    elif task == b'save':
-                        print('Save Checkpoint...')
-                    elif task == b'load':
-                        print('Load Checkpoint...')
-
+                    if b' ' in data:
+                        (action, params) = data.split(b' ')
                     else:
-                        print("NO SUCH ACTION")
+                        action = data
+                        params = b''
+
+                    if action == b'stop':
+                        _shutdown_gracefully(sock)
+                    elif action == b'process':
+
+                        _process_bam(params)
+                    elif action == b'write':
+                        logging.info('Write VCF...', params)
+                        _write_vcf(params)
+                    else:
+                        logging.info("NO SUCH ACTION")
                         # TODO: Throw Illegal Action Exception maybe ?
-
-
-
-
-    if args.stop is not None and sock_running is True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((HOST, PORT))
-            sock.sendall(b'stop')
-            sock.close()
-
-
-
-
-
-    #if args.start is True:
-   #     print("start")
-
     #with daemon.DaemonContext():
-    #    print("LOL")
+    #    logging.info("LOL")
     #serve_forever()
