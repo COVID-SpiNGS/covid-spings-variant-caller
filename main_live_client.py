3891d6ffa38ed84a926b14a2013bd23292390caa
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2022-10-13 10:34:31 +0200

    Fixed CLI for different params

--- main_live_client.py
+++ main_live_client.py
@@ -12,25 +12,26 @@ config = configparser.ConfigParser()
 config.read('sockets.conf')
 HOST = config['BASIC_PARAMS']['HOST']
 PORT = int(config['BASIC_PARAMS']['PORT'])
-print(PORT)
 
 
 def _construct_cli():
     #group = parser.add_mutually_exclusive_group()
     #group.add_argument("stop", nargs='?', help="Shutdown server")
-    parser.add_argument('processing', help='run or stop', nargs='?', choices=('process', 'write'))
-    parser.add_argument('stop', help='run or stop', nargs='?')
+    parser.add_argument('--process', help='run or stop', nargs='+')
+    parser.add_argument('--write', help='run or stop', nargs='+')
+    parser.add_argument('--stop', help='run or stop', nargs='?')
 
 
 def _talk_to_server(action: str, params: str):
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        logging.info(f'Connecting to server under {HOST}:{PORT}...')
-        sock.connect((HOST, PORT))
-        sock.sendall(b'action')
-        sock.close()
-        logging.info(f'Closing connection to server under {HOST}:{PORT}...')
-
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            logging.info(f'Connecting to server under {HOST}:{PORT}...')
+            sock.connect((HOST, PORT))
+            sock.sendall(b'action')
+            sock.close()
+            logging.info(f'Closing connection to server under {HOST}:{PORT}...')
+    except ConnectionRefusedError as ce:
+        logging.error(f'Not able to connect to {HOST}:{PORT}. Is server running?')
 
 if __name__ == '__main__':
     logging.info(f'Welcome... Setting up client')
@@ -39,17 +40,23 @@ if __name__ == '__main__':
     action = ''
     params = ''
 
-    print(args)
+    test = vars(args)
+
+    print(test)
+
     if args.stop is not None:
         action = 'stop'
 
-    if args.processing is not None:
+    if args.process is not None:
         action = 'process'
+        params = args.process[0]
 
-   # if args.write is not None:
-    #    action = 'write'
+    if args.write is not None:
+        action = 'write'
+        params = args.write[0]
 
     logging.info(f'Selected action is {action} with {params}.')
 
-    _talk_to_server(action, params)
+    if action != '':
+        _talk_to_server(action, params)
 
