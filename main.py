8f5efd96a5f8ab3471c34c8fdadf6cd744b5ad4f
Christoph Stach <christoph.stach@gmail.com>
2022-11-16 13:46:14 +0100

    add benchmark

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from variant_caller.live_variant_caller import LiveVariantCaller
 from variant_caller.config import minBaseQuality, minMappingQuality, minTotalDepth
+import time
 
 def main():
+    start = time.time()
+
     minEvidenceDepth = 5
     minEvidenceRatio = 0.0
     maxVariants = 1
@@ -17,22 +20,56 @@ def main():
         maxVariants
     )
 
-    liveVariantCaller.process_bam('input/input.bam')
+    # liveVariantCaller.process_bam('input/input.bam')
 
 
-    # liveVariantCaller.process_bam('input/input-1/input.bam')
-    # liveVariantCaller.process_bam('input/input-2/input.bam')
-    # liveVariantCaller.process_bam('input/input-3/input.bam')
-    # liveVariantCaller.process_bam('input/input-4/input.bam')
-    # liveVariantCaller.process_bam('input/input-5/input.bam')
-    # liveVariantCaller.process_bam('input/input-6/input.bam')
-    # liveVariantCaller.process_bam('input/input-7/input.bam')
-    # liveVariantCaller.process_bam('input/input-8/input.bam')
-    # liveVariantCaller.process_bam('input/input-9/input.bam')
-    # liveVariantCaller.process_bam('input/input-10/input.bam')
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-1/input.bam')
+    print('1', time.time() - start_sub)
+  
+
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-2/input.bam')
+    print('2', time.time() - start_sub)
+
+
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-3/input.bam')
+    print('3', time.time() - start_sub)
+
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-4/input.bam')
+    print('4', time.time() - start_sub)
     
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-5/input.bam')
+    print('5', time.time() - start_sub)
+    
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-6/input.bam')
+    print('6', time.time() - start_sub)
+    
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-7/input.bam')
+    print('7', time.time() - start_sub)
+    
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-8/input.bam')
+    print('8', time.time() - start_sub)
+    
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-9/input.bam')
+    print('9', time.time() - start_sub)
+    
+    start_sub = time.time()
+    liveVariantCaller.process_bam('input/input-10/input.bam')
+    print('10', time.time() - start_sub)
+
+
     liveVariantCaller.write_vcf('output/live_multi_variant_caller.vcf')
-    liveVariantCaller.create_checkpoint('output/checkpoint.pkl')
+    # liveVariantCaller.create_checkpoint('output/checkpoint.pkl')
+
+    print('total', time.time() - start)
   
 
 if __name__=='__main__':
---
+++ plot.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y_python = [
+    5.485137224197388,
+    4.183643102645874, 
+    6.845597505569458,
+    10.174527406692505,
+    14.296257734298706,
+    18.698223114013672,
+    29.482377290725708,
+    36.68915510177612,
+    38.5833375453949,
+    49.222522020339966
+]
+y_kotlin = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10
+]
+
+plt.ylabel('Runtime in seconds')
+plt.xlabel('Number of files')
+plt.plot(x, y_python, label="Python")
+plt.plot(x, y_kotlin, label="Kotlin")
+plt.legend()
+plt.show()
+
