e9f23ef23900677d5cdb12ec834b870818dac7ce
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2023-01-16 21:15:13 +0100

    Added logic for watcher

---
+++ watcher/watcher.py
@@ -0,0 +1,60 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import logging
+import time
+import argparse
+import sys
+import os
+
+logging.basicConfig(filename='../log/watcher.log',
+                    level=logging.DEBUG,
+                    format='%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+
+
+class Watcher:
+
+    def __init__(self, directory, handler=FileSystemEventHandler()):
+        self.observer = Observer()
+        self.handler = handler
+        self.directory = directory
+
+    def run(self):
+        self.observer.schedule(
+            self.handler, self.directory, recursive=True)
+        self.observer.start()
+        logging.info(f'Watcher running in {self.directory}')
+        print(f'Now watching directory {self.directory}')
+        try:
+            while True:
+                time.sleep(1)
+        except:
+            self.observer.stop()
+        self.observer.join()
+        print('Watcher terminated.')
+        logging.info('Watcher terminated.')
+
+
+class MyHandler(FileSystemEventHandler):
+
+    def on_any_event(self, event):
+        print(event)  # Your code here
+
+
+if __name__ == '__main__':
+
+    path = '.'
+
+    if len(sys.argv) > 0:
+        path = sys.argv[1]
+    else:
+        logging.error(f'No path provided')
+        print(f'Please provide path to be watched!')
+
+    if os.path.exists(path) and not os.path.isfile(path):
+        logging.info(f'Provided path: {path}')
+        print(f'Provided path: {path}')
+        w = Watcher(path, MyHandler())
+        w.run()
+    else:
+        logging.error(f'Path {path} does not exist or is a file.')
+        print(f'The provided path - {path} - does not exist or is a file. Path must be a directory!')
