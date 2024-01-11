9d6c9bf8edc6b96c8bb6fc7f8793d9b4b2567711
Vicky Lara Mielczarek <s0558484@htw-berlin.de>
2024-01-11 20:48:49 +0100

    Update

--- src/phred_bayes.py
+++ src/phred_bayes.py
@@ -4,9 +4,9 @@ import variant_caller.utils as u
 
 if __name__ == '__main__':
 
-    single_site_results = [('A',10),('A',5),('G',11),('C',2)]
-    single_site_results = [('C',2),('A',10),('A',5),('G',11)]
-    single_site_results = [('G',3),('C',3),('A',3),('A',3),('A',3)]
+    single_site_results = [('A',25),('A',5),('G',11),('C',2)]
+    single_site_results = [('C',18),('A',10),('A',5),('G',11)]
+    single_site_results = [('G',15),('C',23),('A',3),('A',3),('A',3)]
     
     index_dict = {'A':0,'G':1,'C':2,'T':3}
     
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     
     for read in single_site_results:
         dist = u.update_dist(dist, u.get_likelihood(read))
-        print(dist)
-        print(-np.log(dist)/np.log(10))
+        #print(dist.round(2))
+        print((-np.log(dist)/np.log(10)).round(2))
     
 
-    print(u.compressed_version(single_site_results).round(5))
+    #print(u.compressed_version(single_site_results).round(5))
